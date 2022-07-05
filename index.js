67f35185841d799eb37ed8cb2ee12acc271240c8
Bradley Thomas <bradley.thomas60@googlemail.com>
2022-07-05 11:07:49 +0100

    Forms finally working

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-let noteName = document.getElementById("#title-text-box");
+let noteName = document.getElementById("title-text-box");
 let noteBody = document.getElementById("body-text-box");
-let btn = document.getElementsByClassName("btn");
+let btn = document.getElementById("btn");
 
-let butt = noteName;
+let tester_text = document.getElementById("tester-text")
+let tester_text_2 = document.getElementById("tester-text-2")
 
+btn.addEventListener("click", function() {
+    let _name = noteName.value;
+    let _body = noteBody.value;
 
+    let newNote = new noteTemplate()
 
-$("btn").on('ready', function() {
-    let test = document.getElementById("#tester").innerHTML = butt;
+    // VISUAL REPRESENTATION OF WORKING FORM
+    tester_text.innerText = _name;
+    tester_text_2.innerText = _body;
+    // VISUAL REPRESENTATION OF WORKING FORM
 })
+
+let noteTemplate = {
+    name : _name,
+    body : _body
+}
